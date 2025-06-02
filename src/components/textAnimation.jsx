700f8ecc9c4e14cc49857ad03fd0e7c024bbc65d
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2025-06-03 01:34:16 +0530

    Update textAnimation.jsx

--- src/components/textAnimation.jsx
+++ src/components/textAnimation.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function TextAnimation() {
-  const names = ["Ankit Srivastava","Abhivyakti Srivastava", "Mr. Alok Srivastava", "Jasdeep Singh"];
+  const names = ["Ankit Srivastava","Abhivyakti Srivastava", "Mr. Alok Aviral", "Jasdeep Singh"];
   const designations = [
     "Founder",
     "Co Founder",
